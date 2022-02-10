ebd363f029e92ce2a36a3b9b213b353d4f154fb7
jochen <jochen.christ@innoq.com>
2022-02-10 16:40:19 +0100

    Server Endpoint

---
+++ http-feeds-server/.aws-sam/build/getAllItemsFunction/src/handlers/put-item.js
@@ -0,0 +1,49 @@
+
+const uuid = require('uuid-with-v6');
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.FEED_TABLE;
+
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+exports.putItemHandler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // Get id and name from the body of the request
+    const body = JSON.parse(event.body)
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    // TODO Validierung - aber nicht jetzt :D
+    var params = {
+        TableName : tableName,
+            Item: { id : uuid.v6(),
+                source: body.source,
+                time: new Date().toISOString(),
+                subject: body.subject, 
+                type: body.type,
+                data: JSON.stringify(body.data)
+            }
+    };
+
+    console.info(`persisting ${JSON.stringify(params)}`);
+    const result = await docClient.put(params).promise();
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(body)
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
